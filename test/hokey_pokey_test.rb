bee97de4d2da78d624f3e0a868b275b0b03ec101
Orion Osborn <orion@MacBook-Air-22.local>
2017-03-30 07:45:26 -0600

    initial commit

---
+++ test/hokey_pokey_test.rb
@@ -0,0 +1,108 @@
+require_relative 'test_helper'
+require_relative '../lib/hokey_pokey'
+
+class HokeyPokeyTest < Minitest::Test
+  def test_verse_1
+    expected = <<-VERSE
+You put your right foot in
+You put your right foot out
+You put your right foot in
+And you shake it all about
+You do the Hokey Pokey and you turn yourself around
+That's what it's all about!
+VERSE
+    assert_equal expected, HokeyPokey.new.verse(1)
+  end
+
+  def test_verse_2
+    expected = <<-VERSE
+You put your left foot in
+You put your left foot out
+You put your left foot in
+And you shake it all about
+You do the Hokey Pokey and you turn yourself around
+That's what it's all about!
+VERSE
+    assert_equal expected, HokeyPokey.new.verse(2)
+  end
+
+  def test_verse_3
+    expected = <<-VERSE
+You put your right hand in
+You put your right hand out
+You put your right hand in
+And you shake it all about
+You do the Hokey Pokey and you turn yourself around
+That's what it's all about!
+VERSE
+
+    assert_equal expected, HokeyPokey.new.verse(3)
+  end
+
+  def test_verse_4
+    expected = <<-VERSE
+You put your left hand in
+You put your left hand out
+You put your left hand in
+And you shake it all about
+You do the Hokey Pokey and you turn yourself around
+That's what it's all about!
+VERSE
+
+    assert_equal expected, HokeyPokey.new.verse(4)
+  end
+
+  def test_verse_5
+    expected = <<-VERSE
+You put your head in
+You put your head out
+You put your head in
+And you shake it all about
+You do the Hokey Pokey and you turn yourself around
+That's what it's all about!
+VERSE
+
+    assert_equal expected, HokeyPokey.new.verse(5)
+  end
+
+  def test_the_whole_song
+    expected = <<-SONG
+You put your right foot in
+You put your right foot out
+You put your right foot in
+And you shake it all about
+You do the Hokey Pokey and you turn yourself around
+That's what it's all about!
+
+You put your left foot in
+You put your left foot out
+You put your left foot in
+And you shake it all about
+You do the Hokey Pokey and you turn yourself around
+That's what it's all about!
+
+You put your right hand in
+You put your right hand out
+You put your right hand in
+And you shake it all about
+You do the Hokey Pokey and you turn yourself around
+That's what it's all about!
+
+You put your left hand in
+You put your left hand out
+You put your left hand in
+And you shake it all about
+You do the Hokey Pokey and you turn yourself around
+That's what it's all about!
+
+You put your head in
+You put your head out
+You put your head in
+And you shake it all about
+You do the Hokey Pokey and you turn yourself around
+That's what it's all about!
+SONG
+
+    assert_equal expected, HokeyPokey.new.song
+  end
+end
